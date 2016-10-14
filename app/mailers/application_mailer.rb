b852ab9ca33aa199cc851664428efd83d6404da2
Yung Yuan <leonychuang@gmail.com>
2016-10-14 13:43:01 +0800

    user apply cancel order

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,5 +1,5 @@
 class ApplicationMailer < ActionMailer::Base
   #default from: "service@jdstore.com"
-  default :css => :email, :from => "example@example.com"
+  default :css => :email, :from => "service@jdstore.com"
   layout 'mailer'
 end

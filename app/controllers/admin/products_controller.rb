a65f4cf7d782416e0156ce4dbeaf184c450a78a1
Yung Yuan <leonychuang@gmail.com>
2016-11-02 21:49:20 +0800

    edit/update

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -20,7 +20,16 @@ class Admin::ProductsController < ApplicationController
   def update
     @product = Product.find(params[:id])
 
-    if @product.update(product_params)
+    if params[:photos] != nil
+      @product.photos.destroy_all #need to destroy old pics first
+      params[:photos]['avatar'].each do |a|
+        @picture = @product.photos.create(:avatar => a)
+      end
+
+      @product.update(product_params)
+      redirect_to admin_products_path
+
+    elsif @product.update(product_params)
       redirect_to admin_products_path
     else
       render :edit

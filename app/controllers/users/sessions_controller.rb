46f444c4ccc64366ce6be6e6eda8e4b18f629248
Yung Yuan <leonychuang@gmail.com>
2017-02-23 22:44:27 +0800

    customize devise login

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,4 +1,13 @@
 class Users::SessionsController < Devise::SessionsController
+  prepend_before_action :valify_captcha!, only: [:create]
+
+  def valify_captcha!
+    unless verify_rucaptcha?
+      redirect_to new_user_session_path, alert: t('rucaptcha.invalid')
+      return
+    end
+    true
+  end
 # before_action :configure_sign_in_params, only: [:create]
 
   # GET /resource/sign_in
